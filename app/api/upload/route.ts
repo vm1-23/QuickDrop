18fb8a2572311f033d2e6b679b27001b8d79c95c
vm1-23 <173900329+vm1-23@users.noreply.github.com>
2025-10-16 03:45:13 +0530

    Connected successfully to vercel blob

---
+++ app/api/upload/route.ts
@@ -0,0 +1,57 @@
+import { handleUpload, type HandleUploadBody } from '@vercel/blob/client';
+import { NextResponse } from 'next/server';
+import {put} from "@vercel/blob"
+ 
+export async function POST(request: Request): Promise<NextResponse> {
+  const body = (await request.json()) as HandleUploadBody;
+ 
+  try {
+    console.log('Token found:', !!process.env.BLOB_READ_WRITE_TOKEN);
+
+    const jsonResponse = await handleUpload({
+      body,
+      request,
+      
+      onBeforeGenerateToken: async (
+        pathname,
+        /* clientPayload */
+      ) => {
+        // Generate a client token for the browser to upload the file
+        // Make sure to authenticate and authorize users before generating the token.
+        // Otherwise, you're allowing anonymous uploads.
+ 
+        return {
+          allowedContentTypes: ['image/jpeg', 'image/png', 'image/webp'],
+          addRandomSuffix: true,
+          // callbackUrl: 'https://example.com/api/avatar/upload',
+          // optional, `callbackUrl` is automatically computed when hosted on Vercel
+          tokenPayload: JSON.stringify({
+            // optional, sent to your server on upload completion
+            // you could pass a user id from auth, or a value from clientPayload
+          }),
+        };
+      },
+      onUploadCompleted: async ({ blob, tokenPayload }) => {
+        // Called by Vercel API on client upload completion
+        // Use tools like ngrok if you want this to work locally
+ 
+        console.log('blob upload completed', blob, tokenPayload);
+ 
+        try {
+          // Run any logic after the file upload completed
+          // const { userId } = JSON.parse(tokenPayload);
+          // await db.update({ avatar: blob.url, userId });
+        } catch (error) {
+          throw new Error('Could not update user');
+        }
+      },
+    });
+ 
+    return NextResponse.json(jsonResponse);
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 400 }, // The webhook will retry 5 times waiting for a 200
+    );
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,60 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef,useState } from "react";
 import Image from "next/image";
-import Form from "next/form";
+
 import { Button } from "@/components/ui/button";
+import {upload} from "@vercel/blob/client"
+import { PutBlobResult } from "@vercel/blob";
 
 
 export default function Home() {
-
+  const [progress,setProgress] = useState(0);
+  const [uploading,setUploading] = useState(false); 
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [blobUrl,setBlobUrl] = useState<PutBlobResult | null>(null);
+  const [selectedFileName,setSelectedFileName] = useState<string | null>(null);
 
-  const handleFormSubmit = async (formData: FormData) => {
-
+  const handleFileChange = () =>{
+    if(fileInputRef.current?.files?.[0]){
+      setSelectedFileName(fileInputRef.current?.files?.[0].name);
+    }
+    else{
+      setSelectedFileName(null);
+    }
   }
+
+  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("here")
+    setProgress(0);
+    if(!fileInputRef.current?.files?.[0]){
+      throw new Error("Select a file to upload");
+    }
+
+    const file = fileInputRef.current?.files?.[0];
+    setUploading(true);
+    
+    try{
+      console.log("Uploading file:", file?.name); 
+      const blob = await upload(`${file?.name}`,file,{
+        access: "public",
+        handleUploadUrl: "/api/upload",
+        onUploadProgress: (progressEvent)=>{
+          setProgress(progressEvent.percentage);
+
+
+     }})
+      setBlobUrl(blob);
+      console.log("File uploaded successfully. Blob URL:", blob.url);
+    }catch(error){
+      console.log(error);
+    }finally{
+      setUploading(false);
+    }
+  }
+
+  
   return (
     <>
       <section className="flex bg-slate-100 flex-col items-center justify-center">
@@ -25,14 +67,12 @@ export default function Home() {
           <h2 className="text-2xl font-semibold text-slate-700 mb-5">Upload Your Files</h2>
 
           <div className="w-[100%] flex space-between justify-between">
-            <Form onClick={()=>{fileInputRef.current?.click()
-              console.log(fileInputRef)
-            }} action={handleFormSubmit} className="flex flex-col cursor-pointer items-center justify-around py-8 m-8 w-[80%] md:w-[40%] h-64 border-2 border-dashed border-slate-500 rounded-lg">
-              <input ref={fileInputRef} type="file" className="hidden w-full h-full"></input>
-              <label className="text-lg font-medium cursor-pointer">Choose a file to upload</label>
+            <form onSubmit={handleFormSubmit} className="flex flex-col cursor-pointer items-center justify-around py-8 m-8 w-[80%] md:w-[40%] h-64 border-2 border-dashed border-slate-500 rounded-lg">
+              <input ref={fileInputRef} type="file" required onChange={handleFileChange} className="hidden w-full h-full"></input>
+              <label onClick={()=>fileInputRef.current?.click()} className="text-lg font-medium cursor-pointer">{selectedFileName || "Choose a File to Upload"}</label>
               <p className="text-lg font-medium text-slate-400">Or Drag and Drop</p>
-              <Button  className="bg-red-400">Select File</Button>
-            </Form>
+              <Button type="submit" className="bg-red-400">Select File</Button>
+            </form>
             <div className="hidden md:block">HI</div>
           </div>
         </div>
@@ -41,3 +81,47 @@ export default function Home() {
     </>
   );
 }
+
+// 'use client';
+ 
+// import { type PutBlobResult } from '@vercel/blob';
+// import { upload } from '@vercel/blob/client';
+// import { useState, useRef } from 'react';
+//    import { put } from "@vercel/blob";
+// export default function AvatarUploadPage() {
+
+//   const inputFileRef = useRef<HTMLInputElement>(null);
+//   const [blob, setBlob] = useState<PutBlobResult | null>(null);
+//   return (
+//     <>
+//       <h1>Upload Your Avatar</h1>
+ 
+//       <form
+//         onSubmit={async (event) => {
+//           event.preventDefault();
+ 
+//           if (!inputFileRef.current?.files) {
+//             throw new Error('No file selected');
+//           }
+
+//           const file = inputFileRef.current.files[0];
+ 
+//           const newBlob = await upload(file.name, file, {
+//             access: 'public',
+//             handleUploadUrl: '/api/upload',
+//           });
+ 
+//           setBlob(newBlob);
+//         }}
+//       >
+//         <input name="file" ref={inputFileRef} type="file" required />
+//         <button type="submit">Upload</button>
+//       </form>
+//       {blob && (
+//         <div>
+//           Blob url: <a href={blob.url}>{blob.url}</a>
+//         </div>
+//       )}
+//     </>
+//   );
+// }
